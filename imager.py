9e6d91745406974a29f0adf0cb33ebfab9f2e409
ibnukoel <37127980+ibnukoel@users.noreply.github.com>
2022-08-27 14:45:54 +0700

    add blurring
    add blurring before canny

--- imager.py
+++ imager.py
@@ -10,21 +10,13 @@ def get_data_from_camera(robot,camera):
     """
     img = camera.getImageArray()
     img = numpy.asarray(img, dtype=numpy.uint8)
-    #img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
     img = cv2.flip(cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE),1)
-    #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    #cv2.imwrite("../img1.jpg",img)
-    #return cv2.flip(img, 1)
-
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     lower = numpy.array([0, 93, 0])
     upper = numpy.array([225, 255, 255])
     mask = cv2.inRange(hsv, lower, upper)
-    #cv2.imshow("hsv",mask)
-    #cv2.waitKey()
-    output = cv2.bitwise_and(img,img, mask= mask)
-    #bluring or not
-    edges = cv2.Canny(output,10,150)
+    mask = cv2.GaussianBlur(mask, (5, 5), 0)
+    edges = cv2.Canny(mask,10,150)
     lines = cv2.HoughLines(edges, 1, numpy.pi / 180, 80, None, 0, 0)
     print(lines)
     if lines is not None:
