376bd4c83bcfa441dab22336efa1cbe6ae954c82
ibnukoel <37127980+ibnukoel@users.noreply.github.com>
2023-01-10 02:05:08 +0700

    add map line weight

--- astar.py
+++ astar.py
@@ -18,9 +18,22 @@
 # will have a path from init to goal.
 # ----------
 
-grid = [[0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 1, 0],
-        [0, 0, 1, 0, 1, 0],
+
+#garis di atas = 98,+kanan = 94, +kiri = 93,  +kanan+kiri = 90
+#garis di kanan = 97,+kiri = 86               +atas+bawah = 89
+#garis di bawah = 96,+kanan = 92, +kiri = 91, +kanan+kiri = 88
+#garis di kiri = 95,                          +atas+bawah = 87
+
+
+mapLine = [[93, 98, 98, 98, 98, 94],
+        [91, 96, 96, 96, 96, 97],
+        [1, 1, 1, 1, 1, 86],
+        [0, 0, 0, 0, 1, 86],
+        [0, 0, 1, 0, 1, 88]]
+
+grid = [[0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 0],
         [0, 0, 0, 0, 1, 0],
         [0, 0, 1, 0, 1, 0]]
 init = [0, 0]
@@ -32,7 +45,7 @@ delta = [[-1, 0], # go up
          [1, 0], # go down
          [0, 1]] # go right
 
-#delta_name = ['-90', '180', '90', '0']
+delta_val = ['-90', '180', '90', '0']
 delta_name = ['^', '<', 'v', '>']
 
 def printmap(grid):
@@ -40,20 +53,16 @@ def printmap(grid):
         print(grid[i])
 
 def search(grid,init,goal,cost):
-    # ---------------------------------------- modify code below
-    # ----------------------------------------
     closed = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
     expand = [[' ' for row in range(len(grid[0]))] for col in range(len(grid))]
     action = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
-    urutan = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
+    # action is direction from goal to initial location
 
     closed[init[0]][init[1]] = 1
 
     x = init[0]
     y = init[1]
-    g = 0
-    g2 = 0
-    j = 1
+    g = 1
 
     open = [[g, x, y]]
 
@@ -62,7 +71,7 @@ def search(grid,init,goal,cost):
 
     while not found and not resign:
         if len(open) == 0:
-            resign = True
+            #resign = True
             return 'fail'
         else:
             open.sort()
@@ -70,7 +79,6 @@ def search(grid,init,goal,cost):
             next = open.pop()
             x = next[1]
             y = next[2]
-            g = next[0]
             if x == goal[0] and y == goal[1]:
                 found = True
             else:
@@ -79,23 +87,29 @@ def search(grid,init,goal,cost):
                     y2 = y + delta[i][1]
                     if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
                         if closed[x2][y2] == 0 and grid[x2][y2] == 0:
-                            g2 = g2 + cost
-                            open.append([g2, x2, y2])
-                            closed[x2][y2] = g2
+                            g = g + cost
+                            open.append([g, x2, y2])
+                            closed[x2][y2] = g
                             action[x2][y2] = i
-                            g = g2
+                            #g = g2
 
+    #creat path from action / direction
     x = goal[0]
     y = goal[1]
     expand[x][y]="*"
     while x !=init[0] or y!=init[1]:
+        # movement configuration :
+        # urutan,next move,robot facing, x , y , deltaRho1, deltaRho2,rhomin)
+        #movement = [0, z, x, y, 0, 0, 0]
         x2 = x - delta[action[x][y]][0]
         y2 = y - delta[action[x][y]][1]
         expand[x2][y2] = delta_name[action[x][y]]
         x = x2
         y = y2
-    printmap(closed)
-    return expand #return expand # make sure you return the shortest path
 
-newpath = search(grid,init,goal,cost)
+    return action,expand #return expand # make sure you return the shortest path
+
+action,newpath = search(grid,init,goal,cost)
 printmap(newpath)
+printmap(action)
+
