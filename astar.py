fcad1a2fe43812f3d51551514d345a6641c04b4f
ibnukoel <37127980+ibnukoel@users.noreply.github.com>
2023-01-10 22:11:15 +0700

    Update astar.py

--- astar.py
+++ astar.py
@@ -36,16 +36,20 @@ grid = [[0, 0, 0, 0, 0, 0],
         [1, 1, 1, 1, 1, 0],
         [0, 0, 0, 0, 1, 0],
         [0, 0, 1, 0, 1, 0]]
+
 init = [0, 0]
 goal = [len(grid)-1, len(grid[0])-1]
+goal_dir = 0
 cost = 1
+dp1 = 14
+dp2 = 2
 
 delta = [[-1, 0], # go up
          [0, -1], # go left
          [1, 0], # go down
          [0, 1]] # go right
 
-delta_val = ['-90', '180', '90', '0']
+delta_val = [-90, 180, 90, 0]
 delta_name = ['^', '<', 'v', '>']
 
 def printmap(grid):
@@ -98,9 +102,6 @@ def search(grid,init,goal,cost):
     y = goal[1]
     expand[x][y]="*"
     while x !=init[0] or y!=init[1]:
-        # movement configuration :
-        # urutan,next move,robot facing, x , y , deltaRho1, deltaRho2,rhomin)
-        #movement = [0, z, x, y, 0, 0, 0]
         x2 = x - delta[action[x][y]][0]
         y2 = y - delta[action[x][y]][1]
         expand[x2][y2] = delta_name[action[x][y]]
@@ -113,3 +114,82 @@ action,newpath = search(grid,init,goal,cost)
 printmap(newpath)
 printmap(action)
 
+def deltaRho_cal(x,y,z):
+    ML = mapLine[x][y]
+    # garis di atas = 98,+kanan = 94, +kiri = 93,  +kanan+kiri = 90
+    # garis di kanan = 97,+kiri = 86               +atas+bawah = 89
+    # garis di bawah = 96,+kanan = 92, +kiri = 91, +kanan+kiri = 88
+    # garis di kiri = 95,                          +atas+bawah = 87
+    if z == 0:
+        #if ML == 98 or ML == 96 or ML == 95 or ML == 93 or ML == 91 or ML == 87:
+        if ML == 97 or ML == 94 or ML == 92 or ML == 90 or ML == 89 or ML == 86 or ML == 88:
+            dp = dp1
+        else: dp = 0
+    elif z == 90:
+        if ML == 89 or ML == 96 or ML == 92 or ML == 91 or ML == 88 or ML == 87:
+            dp = dp1
+        else: dp = 0
+    elif z == 180:
+        if ML == 93 or ML == 90 or ML == 86 or ML == 91 or ML == 95 or ML == 87 or ML == 88:
+            dp = dp1
+        else: dp = 0
+    elif z == -90:
+        if ML == 98 or ML == 90 or ML == 94 or ML == 93 or ML == 89 or ML == 87:
+            dp = dp1
+        else: dp = 0
+
+    x_2 = x+1
+    y_2 = y+1
+    if dp == 0 and x_2 < len(mapLine) and y_2 < len(mapLine[1]):
+        ML = mapLine[x+1][y+1]
+        # garis di atas = 98,+kanan = 94, +kiri = 93,  +kanan+kiri = 90
+        # garis di kanan = 97,+kiri = 86               +atas+bawah = 89
+        # garis di bawah = 96,+kanan = 92, +kiri = 91, +kanan+kiri = 88
+        # garis di kiri = 95,                          +atas+bawah = 87
+        if z == 0:
+            # if ML == 98 or ML == 96 or ML == 95 or ML == 93 or ML == 91 or ML == 87:
+            if ML == 97 or ML == 94 or ML == 92 or ML == 90 or ML == 89 or ML == 86 or ML == 88:
+                dp = dp2
+            else:
+                dp = 0
+        elif z == 90:
+            if ML == 89 or ML == 96 or ML == 92 or ML == 91 or ML == 88 or ML == 87:
+                dp = dp2
+            else:
+                dp = 0
+        elif z == 180:
+            if ML == 93 or ML == 90 or ML == 86 or ML == 91 or ML == 95 or ML == 87 or ML == 88:
+                dp = dp2
+            else:
+                dp = 0
+        elif z == -90:
+            if ML == 98 or ML == 90 or ML == 94 or ML == 93 or ML == 89 or ML == 87:
+                dp = dp2
+            else:
+                dp = 0
+
+    return dp
+
+
+# movement configuration :
+# urutan,next move,robot facing, x , y , deltaRho1, deltaRho2,rhomin)
+# movement = [0, z, x, y, 0, 0, 0]
+x = goal[0]
+y = goal[1]
+z2 = goal_dir
+z1 = delta_val[action[x][y]]
+dpA = deltaRho_cal(x, y, z1)
+dpB = deltaRho_cal(x, y, z2)
+movement = [[0, z2, x, y, dpA, dpB]]
+i = 1
+while x !=init[0] or y!=init[1]:
+    x = x - delta[action[x][y]][0]
+    y = y - delta[action[x][y]][1]
+    z2 = z1
+    z1 = delta_val[action[x][y]]
+    dpA = deltaRho_cal(x, y, z1)
+    dpB = deltaRho_cal(x, y, z2)
+    movement.append([i, z2, x, y, dpA, dpB])
+    i = i+1
+
+print("test",movement)
